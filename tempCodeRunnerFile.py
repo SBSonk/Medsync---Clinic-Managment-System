528cb4ecc1c36727805028bc3d1e907f0b1aa0d1
Noah <noah.c.penaranda@gmail.com>
2025-03-29 14:21:19 +0800

    added react client

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+@app.route("/profile")
+# @app.route("/profile/<name>")
+# def profile(name=None):
+#     if name not in names:
+#         return render_template("404.html") 
+#     else:
+#         return render_template('profile.html', person=name)
