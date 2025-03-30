e0af144538e0b641ff251f6a1a88cb096eac0dc2
Stanley Buenaventura <105530737+SBSonk@users.noreply.github.com>
2025-03-30 22:59:43 +0800

    default admin acc

--- server/main.py
+++ server/main.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
-from models import db, bcrypt
+from models import db, bcrypt, User
 from routes import api
 from flask_jwt_extended import JWTManager
 import database_defaults
@@ -16,9 +16,14 @@ jwt = JWTManager(app)
 bcrypt.init_app(app)
 db.init_app(app)
 with app.app_context():
-    db.create_all()
-
-    database_defaults.populate_people(db)
-
+    inspector = db.inspect(db.engine)
+    
+    if "person" not in inspector.get_table_names():  # 🔹 Check for a specific table
+        db.create_all()
+        database_defaults.populate_people(db)
+        default_admin = User('admin', 'tite', 'master betlog', 2)
+        db.session.add(default_admin)
+        db.session.commit()
+        
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
